27ec4edb52f0b6bb78f75eddfb09a7580096bd92
HAlGhanim <imzaji98@gmail.com>
2025-02-16 18:33:41 +0300

    Done with the task and challenge

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    val hannibal = Book("The Silence of the Lambs", "Thomas Harris", 1988, "Thriller")
+    hannibal.printBookInfo()
+}
+
+class Book(var title: String, var author: String, var yearPublished: Int, var genre: String) {
+    fun printBookInfo() {
+        println("$title By $author ($yearPublished)\nGenre: $genre")
+    }
 }
